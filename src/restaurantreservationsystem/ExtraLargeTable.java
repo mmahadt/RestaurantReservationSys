788f6f297fb80cb3577b544445ad62c0eb6e44a2
Muhammad Mahad Tariq <mmahad.bese14seecs@nust.edu.pk>
2017-03-08 16:28:04 +0500

    fix: fixed bugs

--- src/restaurantreservationsystem/ExtraLargeTable.java
+++ src/restaurantreservationsystem/ExtraLargeTable.java
@@ -5,10 +5,40 @@
  */
 package restaurantreservationsystem;
 
+import java.util.Scanner;
+
 /**
  *
  * @author mmahad.bese14seecs
  */
-public class ExtraLargeTable {
-    
+public class ExtraLargeTable implements Table{
+
+    boolean booked = false;//not booked yet
+
+    public boolean isFull() {
+        return booked;
+    }
+
+    @Override
+    public boolean getATableBooked() {
+
+        if (!this.isFull()) {
+            Customer C1 = new Customer();
+
+            for (int i = 0; i < 1; i++) {
+                System.out.println("Please tell your preferable time slot.");
+                
+                Scanner scan = new Scanner(System.in);
+                
+                String time=scan.nextLine();
+                
+                C1.setBookingDetails(time, i, "ExtraLarge Table");
+                C1.getDetails();
+                System.out.println("Congratulations your table has been booked.");
+                return true;
+            }
+        }
+        System.out.println("We are sorry. Extra large table has already been booked by someone for today.");
+        return false;
+    }
 }
